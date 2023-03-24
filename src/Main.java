59b9fa404bc29d3f59bf5f8bf38c6d24e6a65baa
Galina <serena_05@mail.ru>
2023-03-24 16:15:03 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,133 +1,75 @@
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static StringBuilder sbd = new StringBuilder();
+    public static StringBuilder sbf = new StringBuilder();
 
-        File dir1 = new File("D:/Games//crs");
-
-        if (dir1.mkdir())
-            System.out.println("Каталог создан");
-        else
-            System.out.println("Что-то пошло не так");
+    public static void main(String[] args) {
 
 
-        File dir2 = new File("D:/Games//res");
+        List<String> namesDir = Arrays.asList("D:\\Games\\crs", "D:\\Games\\res", "D:\\Games\\savegames", "D:\\Games\\temp", "D:\\Games\\crs\\main", "D:\\Games\\crs\\test",
+                "D:\\Games\\res\\drawables", "D:\\Games\\res\\vectors", "D:\\Games\\res\\icons");
 
-        if (dir2.mkdir())
-            System.out.println("Каталог создан");
-        else
-            System.out.println("Что-то пошло не так");
+        List<String> files = Arrays.asList("D:\\Games\\crs\\main\\Main.java", "D:\\Games\\temp\\temp.txt", "D:\\Games\\crs\\main\\Utils.java");
 
+        for (String names : namesDir) {
+            createDirectory(String.valueOf(names));
 
-        File dir3 = new File("D:/Games//savegames");
-
-        if (dir3.mkdir())
-            System.out.println("Каталог создан");
-        else
-            System.out.println("Что-то пошло не так");
-
-        File dir4 = new File("D:/Games//temp");
-
-        if (dir4.mkdir())
-            System.out.println("Каталог создан");
-        else
-            System.out.println("Что-то пошло не так");
-
-        File dir5 = new File("D:/Games//crs/main");
-
-        if (dir5.mkdir())
-            System.out.println("Каталог создан");
-        else
-            System.out.println("Что-то пошло не так");
-
-        File dir6 = new File("D:/Games//crs/test");
-
-        if (dir6.mkdir())
-            System.out.println("Каталог создан");
-        else
-            System.out.println("Что-то пошло не так");
-
-        File dir7 = new File("D:/Games//crs/main/Main.java");
-        try {
-            if (dir7.createNewFile())
-                System.out.println("Файл создан");
-            else
-                System.out.println("Что-то пошло не так");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
 
-        File dir8 = new File("D:/Games//crs/main/Utils.java");
-        try {
-            if (dir8.createNewFile())
-                System.out.println("Файл создан");
-            else
-                System.out.println("Что-то пошло не так");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        for (String file : files) {
+            createNewFile(String.valueOf(file));
+
         }
 
-        File dir9 = new File("D:/Games//res/drawables");
-
-        if (dir9.mkdir())
-            System.out.println("Каталог создан");
-        else
-            System.out.println("Что-то пошло не так");
-
-        File dir10 = new File("D:/Games//res/vectors");
-
-        if (dir10.mkdir())
-            System.out.println("Каталог создан");
-        else
-            System.out.println("Что-то пошло не так");
-
-        File dir11 = new File("D:/Games//res/icons");
-
-        if (dir11.mkdir())
-            System.out.println("Каталог создан");
-        else
-            System.out.println("Что-то пошло не так");
-
-        File dir12 = new File("D:/Games//temp/temp.txt");
-        try {
-            if (dir12.createNewFile())
-                System.out.println("Файл создан");
-            else
-                System.out.println("Что-то пошло не так");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(dir1 + "\n");
-        sb.append(dir2 + "\n");
-        sb.append(dir3 + "\n");
-        sb.append(dir4 + "\n");
-        sb.append(dir5 + "\n");
-        sb.append(dir6 + "\n");
-        sb.append(dir7 + "\n");
-        sb.append(dir8 + "\n");
-        sb.append(dir9 + "\n");
-        sb.append(dir10 + "\n");
-        sb.append(dir11 + "\n");
-        sb.append(dir12 + "\n");
-
-
-
-        try(FileWriter writer = new FileWriter("D:/Games//temp/temp.txt", false))
-        {
-            String text = sb.toString();
+        try (FileWriter writer = new FileWriter("D:/Games//temp/temp.txt", false)) {
+            String text = sbd.toString();
             writer.write(text);
-        }
-        catch(IOException ex){
+            String text1 = sbf.toString();
+            writer.write(text1);
+        } catch (IOException ex) {
 
             System.out.println(ex.getMessage());
         }
 
-
     }
+
+
+    public static void createDirectory(String names) {
+
+
+        File dir = new File(names);
+        sbd.append("Директория " + dir.getName());
+        if (dir.mkdir()) {
+            sbd.append(" успешно создана");
+        } else {
+            sbd.append(" не создана");
+        }
+        sbd.append("\n");
+    }
+
+    public static void createNewFile(String file) {
+
+        File dir = new File(file);
+        try {
+            if (dir.createNewFile())
+                sbf.append("Файл создан " + dir.getName());
+            else
+                sbf.append("Что-то пошло не так");
+            sbf.append("\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
+
+
+
+
